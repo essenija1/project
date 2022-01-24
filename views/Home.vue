10e4baa5bd7fdd10ccc8a1d125f311edfe5fc6b6
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-01-24 11:45:38 +0300

    Create Home.vue

---
+++ views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+        <p>Text text text</p>
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    //HelloWorld
+  }
+}
+</script>
