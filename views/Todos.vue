79565a9c96892916450ec08772388e35d81550e9
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-01-24 11:46:01 +0300

    Create Todos.vue

---
+++ views/Todos.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h2>Todo application</h2>
+    <router-link to="/">Home</router-link>
+    <AddTodo 
+    @add-todo="addTodo" 
+    />
+    
+    <hr>
+    <TodoList 
+    v-if="todos.length"
+    v-bind:todos="todos" 
+    @remove-todo="removeTodo" />
+    <p v-else>No todos!</p>
+  </div>
+</template>
+
+<script>
+import TodoList from "@/components/TodoList";
+import AddTodo from "@/components/AddTodo";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  mounted() {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=3")
+    .then((response) => response.json())
+    .then(json => {
+      this.todos = json
+    }) 
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter((t) => t.id !== id);
+    },
+    addTodo(todo) {
+      this.todos.push(todo);
+    },
+  },
+  components: {
+    TodoList,
+    AddTodo
+  
+  },
+};
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
+
+
+
